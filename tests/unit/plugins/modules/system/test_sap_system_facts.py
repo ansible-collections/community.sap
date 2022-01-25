2bb29c0b03e643e4e1a5d00bc962511583f66822
Rainer Leber <rainer.leber@sva.de>
2022-01-25 20:47:51 +0000

    more complex test

--- tests/unit/plugins/modules/system/test_sap_system_facts.py
+++ tests/unit/plugins/modules/system/test_sap_system_facts.py
@@ -4,19 +4,18 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
+
+import mock
 __metaclass__ = type
 
 from ansible_collections.community.sap.plugins.modules.system import sap_system_facts
-from ansible_collections.community.sap.tests.unit.plugins.modules.utils import AnsibleExitJson, AnsibleFailJson, ModuleTestCase
-from ansible_collections.community.sap.tests.unit.compat.mock import patch
+from ansible_collections.community.sap.tests.unit.plugins.modules.utils import AnsibleExitJson, ModuleTestCase
+from ansible_collections.community.sap.tests.unit.compat.mock import patch, MagicMock
 from ansible.module_utils import basic
 
-
 def get_bin_path(*args, **kwargs):
-    """Function to return path of SAPCAR"""
+    """Function to return path of sapcontrol"""
     return "/usr/sap/hostctrl/exe/sapcontrol"
-
-
 class Testsap_system_facts(ModuleTestCase):
     """Main class for testing sap_system_facts module."""
 
@@ -24,45 +23,23 @@ class Testsap_system_facts(ModuleTestCase):
         """Setup."""
         super(Testsap_system_facts, self).setUp()
         self.module = sap_system_facts
+        self.mock_get_bin_path = patch.object(basic.AnsibleModule, 'get_bin_path', get_bin_path)
+        self.mock_get_bin_path.start()
+        self.addCleanup(self.mock_get_bin_path.stop) 
+        
 
     def tearDown(self):
         """Teardown."""
         super(Testsap_system_facts, self).tearDown()
 
     def test_no_systems_available(self):
-        """Failure must occurs when all parameters are missing."""
-        with patch.object(basic.AnsibleModule, 'run_command') as run_command:
-            run_command.return_value = 1, '', ''
-            with self.assertRaises(AnsibleExitJson) as result:
-                self.module.main()
+        """No SAP Systems"""
+        with self.assertRaises(AnsibleExitJson) as result:
+            self.module.main()
         self.assertEqual(result.exception.args[0]['ansible_facts'], {})
 
-    def test_sap_system_facts_hana(self):
-        """Check that result is changed when HANA system."""
-        with patch.object(self.module, 'get_all_hana_sid') as get_all_hana_sid:
-            get_all_hana_sid.return_value = ['HDB']
-            with patch.object(self.module, 'get_hana_nr') as get_hana_nr:
-                get_hana_nr.return_value = [{"InstanceType": "HANA", "NR": "01", "SID": "HDB", "TYPE": "HDB"}]
-                with self.assertRaises(AnsibleExitJson) as result:
-                    self.module.main()
-                    self.assertTrue(result.exception.args[0]['changed'])
-        self.assertEqual(result.exception.args[0]['ansible_facts'], {'sap': [{"InstanceType": "HANA", "NR": "01", "SID": "HDB", "TYPE": "HDB"}]})
-
-    def test_sap_system_facts_nw(self):
-        """Check that result is changed when NW system."""
-        with patch.object(self.module, 'get_all_nw_sid') as get_all_nw_sid:
-            get_all_nw_sid.return_value = ['ABC']
-            with patch.object(self.module, 'get_nw_nr') as get_nw_nr:
-                get_nw_nr.return_value = [{"InstanceType": "NW", "NR": "00", "SID": "ABC", "TYPE": "ASCS"},
-                                          {"InstanceType": "NW", "NR": "01", "SID": "ABC", "TYPE": "PAS"}]
-                with self.assertRaises(AnsibleExitJson) as result:
-                    self.module.main()
-                    self.assertTrue(result.exception.args[0]['changed'])
-        self.assertEqual(result.exception.args[0]['ansible_facts'], {'sap': [{"InstanceType": "NW", "NR": "00", "SID": "ABC", "TYPE": "ASCS"},
-                                                                             {"InstanceType": "NW", "NR": "01", "SID": "ABC", "TYPE": "PAS"}]})
-
     def test_sap_system_facts_all(self):
-        """Check that result is changed when NW system."""
+        """Check that result is changed when all is one system."""
         with patch.object(self.module, 'get_all_hana_sid') as get_all_hana_sid:
             get_all_hana_sid.return_value = ['HDB']
             with patch.object(self.module, 'get_hana_nr') as get_hana_nr:
@@ -74,7 +51,30 @@ class Testsap_system_facts(ModuleTestCase):
                                                   {"InstanceType": "NW", "NR": "01", "SID": "ABC", "TYPE": "PAS"}]
                         with self.assertRaises(AnsibleExitJson) as result:
                             self.module.main()
-                            self.assertTrue(result.exception.args[0]['changed'])
         self.assertEqual(result.exception.args[0]['ansible_facts'], {'sap': [{"InstanceType": "HANA", "NR": "01", "SID": "HDB", "TYPE": "HDB"},
                                                                              {"InstanceType": "NW", "NR": "00", "SID": "ABC", "TYPE": "ASCS"},
                                                                              {"InstanceType": "NW", "NR": "01", "SID": "ABC", "TYPE": "PAS"}]})
+    
+    def test_sap_system_facts_command_hana(self):
+        """Check that result for HANA is correct."""
+        with patch.object(self.module, 'get_all_hana_sid') as mock_all_hana_sid:
+            mock_all_hana_sid.return_value = ['HDB']
+            with patch.object(self.module.os, 'listdir') as mock_listdir:
+                mock_listdir.return_value = ['HDB01']
+                with patch.object(basic.AnsibleModule, 'run_command') as run_command:
+                    run_command.return_value = [0, '', '']  # successful execution, no output
+                    with self.assertRaises(AnsibleExitJson) as result:
+                        self.module.main()
+        self.assertEqual(result.exception.args[0]['ansible_facts'], {'sap': [{"InstanceType": "HANA", "NR": "01", "SID": "HDB", "TYPE": "HDB"}]})
+
+    def test_sap_system_facts_command_nw(self):
+        """Check that result for NW is correct."""
+        with patch.object(self.module, 'get_all_nw_sid') as mock_all_nw_sid:
+            mock_all_nw_sid.return_value = ['ABC']
+            with patch.object(self.module.os, 'listdir') as mock_listdir:
+                mock_listdir.return_value = ['ASCS00']
+                with patch.object(basic.AnsibleModule, 'run_command') as run_command:
+                    run_command.return_value = [0, 'SAP\nINSTANCE_NAME, Attribute, D00\nSAP', '']  # successful execution, no output
+                    with self.assertRaises(AnsibleExitJson) as result:
+                        self.module.main()
+        self.assertEqual(result.exception.args[0]['ansible_facts'], {'sap': [{'InstanceType': 'NW', 'NR': '00', 'SID': 'ABC', 'TYPE': 'PAS'}]})
